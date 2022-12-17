d67009489c3eabb5b107560035e55f8f6f19d1b4
DadyaBorya <julijaogr@ukr.net>
2022-12-17 19:45:41 +0200

    Task_2.3(1) is ready

---
+++ js/task_2.3/task_1/Main.js
@@ -0,0 +1,15 @@
+const snakeNames = require('snake-names');
+const nuzhdiki = require('nuzhdiki');
+const player = require('play-sound')();
+
+function play() {
+    console.log("Приходит в бар -", snakeNames.random(), "и говорит")
+    console.log(nuzhdiki.getNuzhdik().nuzhdik)
+
+
+
+     setTimeout( () => player.play('sounds/smeh-1.mp3', { timeout: 30000 }, function(err){
+        if (err) console.log(`Could not play sound: ${err}`);
+    }), 4000)
+}
+play()
